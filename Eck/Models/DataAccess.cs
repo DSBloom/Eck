6f526665753dce868fd44f7f55f19a9bf5439892
David Bloom <nadcraker@gmail.com>
2015-05-23 18:25:06 -0700

    Added Actions to insert and list data. Added data classes for test data.

---
+++ Eck/Models/DataAccess.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Eck.Models
+{
+    public class DataAccess
+    {
+        public  static SqlConnection ConnectionString { get; set; }
+
+        internal static void InsertTestData(TestData model)
+        {
+            SqlConnection conn = new SqlConnection(@"Server=tcp:ye8viai1bq.database.windows.net,1433;Database=Eck01_db;User ID=nadcraker@ye8viai1bq;Password=Fallout!@#$;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            string insertText = @"INSERT INTO Teams (TeamName, TeamOwner, Digit00, Digit01, Digit02, Digit03, Digit04, Digit05, Digit06, Digit07, Digit08, Digit09, TeamOwnerEmail) " +
+                                 "VALUES (@TeamName, @TeamOwner, @Digit00, @Digit01, @Digit02, @Digit03, @Digit04, @Digit05, @Digit06, @Digit07, @Digit08, @Digit09, @TeamOwnerEmail)";
+            SqlCommand cmd = new SqlCommand(insertText, conn);
+            cmd.Parameters.AddWithValue("TeamName", model.TeamName);
+            cmd.Parameters.AddWithValue("TeamOwner", model.TeamOwner);
+            cmd.Parameters.AddWithValue("TeamOwnerEmail", model.TeamOwnerEmail);
+            cmd.Parameters.AddWithValue("Digit00", model.Digit00);
+            cmd.Parameters.AddWithValue("Digit01", model.Digit01);
+            cmd.Parameters.AddWithValue("Digit02", model.Digit02);
+            cmd.Parameters.AddWithValue("Digit03", model.Digit03);
+            cmd.Parameters.AddWithValue("Digit04", model.Digit04);
+            cmd.Parameters.AddWithValue("Digit05", model.Digit05);
+            cmd.Parameters.AddWithValue("Digit06", model.Digit06);
+            cmd.Parameters.AddWithValue("Digit07", model.Digit07);
+            cmd.Parameters.AddWithValue("Digit08", model.Digit08);
+            cmd.Parameters.AddWithValue("Digit09", model.Digit09);
+
+            try
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ Eck/Models/TestData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eck.Models
+{
+    public class TestData
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public string TeamOwner { get; set; }
+        public string TeamOwnerEmail { get; set; }
+        public bool Digit00 { get; set; }
+        public bool Digit01 { get; set; }
+        public bool Digit02 { get; set; }
+        public bool Digit03 { get; set; }
+        public bool Digit04 { get; set; }
+        public bool Digit05 { get; set; }
+        public bool Digit06 { get; set; }
+        public bool Digit07 { get; set; }
+        public bool Digit08 { get; set; }
+        public bool Digit09 { get; set; }
+    }
+}
